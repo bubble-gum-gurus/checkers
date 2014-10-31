53b38875418653d16d45d9c82520e2837759737b
jo-crawf <thedevelopmentprocess@gmail.com>
2014-10-31 17:14:49 -0400

    skeleton of listuserstats NOTDONE

--- src/org/soen387/app/ViewUserStats.java
+++ src/org/soen387/app/ViewUserStats.java
@@ -1,5 +1,29 @@
 package org.soen387.app;
 
-public class ViewUserStats {
+import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dsrg.soenea.domain.MapperException;
+import org.soen387.domain.user.mapper.UserDataMapper;
+import org.soen387.domain.model.user.IUser;
+import org.soen387.domain.model.user.User;
+
+public class ViewUserStats extends AbstractPageController implements Servlet{
+
+	public ViewUserStats()
+	{
+		super();
+	}
+	
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+	}
 }
